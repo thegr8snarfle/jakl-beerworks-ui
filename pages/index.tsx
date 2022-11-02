b8d5ad6fe3ce42c0a56ac35f993bfa351b1b8444
Austin Kulp <austin.kulp@gmail.com>
2022-11-01 22:22:05 -0600

    Add favicon.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import fourthSlide from '../assets/image/taps.jpg';
 import {Carousel, Container, Row, Col} from "react-bootstrap";
 import {ReactElement} from "react";
 import {NextPageWithLayout} from "@model/common.model";
+import Head from "next/head";
 
 const Home: NextPageWithLayout = () => {
   const images: { [key: string]: any } = {
@@ -15,26 +16,33 @@ const Home: NextPageWithLayout = () => {
   };
 
   return (
-    <Container>
-      <Row>
-        <Col className="align-items-center">
-          <Carousel className="w-100" fade={true} pause="hover" interval={5000}>
-            <Carousel.Item className="text-center">
-              <img height={600} width={600} src={images['first'].src}/>
-            </Carousel.Item>
-            <Carousel.Item className="text-center">
-              <img height={600} width={600} src={images['second'].src} />
-            </Carousel.Item>
-            <Carousel.Item className="text-center">
-              <img height={600} width={600} src={images['third'].src} />
-            </Carousel.Item>
-            <Carousel.Item className="text-center">
-              <img height={600} width={600} src={images['fourth'].src} />
-            </Carousel.Item>
-        </Carousel>
-       </Col>
-      </Row>
-    </Container>
+    <>
+      <Head>
+        <title>JAKL Beerworks</title>
+        <link href="/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+      </Head>
+      <Container>
+        <Row>
+          <Col className="align-items-center">
+            <Carousel className="w-100" fade={true} pause="hover" interval={5000}>
+              <Carousel.Item className="text-center">
+                <img height={600} width={600} src={images['first'].src}/>
+              </Carousel.Item>
+              <Carousel.Item className="text-center">
+                <img height={600} width={600} src={images['second'].src} />
+              </Carousel.Item>
+              <Carousel.Item className="text-center">
+                <img height={600} width={600} src={images['third'].src} />
+              </Carousel.Item>
+              <Carousel.Item className="text-center">
+                <img height={600} width={600} src={images['fourth'].src} />
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
+    </>
+
   )
 }
 
