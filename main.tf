51aa097e9926a3376e5d1a67bbc142569347d160
Austin Kulp <austin.kulp@gmail.com>
2022-11-01 21:52:54 -0600

    Add elastic ip mapping and backend config to terraform, add terraform to circleci.

--- main.tf
+++ main.tf
@@ -1,10 +1,21 @@
 ##################################################################################
+# STATE
+##################################################################################
+terraform {
+  backend "s3" {
+    bucket = "jakl-tf-state"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+##################################################################################
 # PROVIDERS
 ##################################################################################
 
 provider "aws" {
-  access_key = "AKIARJXCRRNHDDDAGGY3"
-  secret_key = "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"
+  access_key = "AKIARJXCRRNHIIUEKSZ6"
+  secret_key = "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"
   region     = "us-west-2"
 }
 
@@ -21,7 +32,7 @@ data "aws_ami" "amazonLinux" {
   owners        = ["amazon"]
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-gp2"]
+    values = ["amzn2-ami-ecs-hvm-2.0.20221025-x86_64-ebs"]
   }
 
   filter {
@@ -40,6 +51,13 @@ data "aws_ami" "amazonLinux" {
   }
 }
 
+data "aws_eip" "web-eip" {
+  filter {
+    name   = "tag:Name"
+    values = ["web-eip"]
+  }
+}
+
 resource "aws_key_pair" "keypair" {
   key_name   = "jakl-web-deploy"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9wK6Gu3vtPar0rQ+DeBVMrTxMo8sT++KYGj5NLUHhS3ckuELd5fgKm348lsBnyZk4eg/+CFO2vq1+dp9MvgORCA/FCUHOzHnm98pBTNrkQEMCV855JZUJdrFXApL7sbVkiDZWBN20MsoasLeKwb1Dqa+9W8QnmZKY6HkMio6GumdA1I6mEM531mexrarWm9WEe0Xr27+F7KeQOeDkCOYXduuEXfVMhurabTazNTBhLPb2mV+bWpwqLFGkHlcJTOPu+4YRGI+jpFurVw05QyJHZOaLSZqj1zUyTLI28tUlvQrltbEl5d0Ly8uTeatTIzCjIF0TIgTB52tLS7hoSetYKueN2YfWTjnhZix4VX+aJco6lvuvLQikwO9mDGgqjwvdajJVOkBFL+qgzVISkM4krvQYzR5MasCUAmG5QJaCfO7EARa9mTSCFXcma+nqYgQA+UNBT77xhi1hlAVwX+/oGPFg34CMHuN3fWC+n2wtnh5ENGL8kDRHfc1i4+rhK65paQ9vJZXo5BscTxJ/mQnupfI6ZKQdAi8hDTOEknZvFSZr7+JbuXL/4IX7vs+M1r71+WPLQpkBuRawm0xo0noQERsGW31bh1dhoqv7JpqnANbvd4s4+zOrDS96AZ8RMqPHs4Bq87TOEyPOS9PeLvBenyev7L5wlu4DZSnXjffOMw== austin@jerry.local"
@@ -53,23 +71,39 @@ resource "aws_key_pair" "keypair" {
 resource "aws_vpc" "vpc-auto" {
   cidr_block           = "10.1.0.0/16"
   enable_dns_hostnames = true
-
+  tags = {
+    name: "web-vpc-auto"
+  }
 }
 
 resource "aws_internet_gateway" "igw-auto" {
   vpc_id = aws_vpc.vpc-auto.id
-
+  tags = {
+    name: "web-igw-auto"
+  }
 }
 
 resource "aws_subnet" "web-subnt-auto" {
   cidr_block              = "10.1.254.0/24"
   vpc_id                  = aws_vpc.vpc-auto.id
   map_public_ip_on_launch = true
+  tags = {
+    name: "web-subnt-auto"
+  }
+}
+
+// already exists on the server
+resource "aws_eip_association" "staging_eip" {
+  instance_id   = aws_instance.jakl-web-auto.id
+  allocation_id = data.aws_eip.web-eip.id
 }
 
 # ROUTING #
-resource "aws_route_table" "rtb" {
+resource "aws_route_table" "rtb-auto" {
   vpc_id = aws_vpc.vpc-auto.id
+  tags = {
+    name: "web-rt-auto"
+  }
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -79,13 +113,16 @@ resource "aws_route_table" "rtb" {
 
 resource "aws_route_table_association" "rta-subnet1" {
   subnet_id      = aws_subnet.web-subnt-auto.id
-  route_table_id = aws_route_table.rtb.id
+  route_table_id = aws_route_table.rtb-auto.id
 }
 
 # SECURITY GROUPS #
 resource "aws_security_group" "web-sg-auto" {
   name   = "web-sg-auto"
   vpc_id = aws_vpc.vpc-auto.id
+  tags = {
+    name: "web-sg-auto"
+  }
 
   # HTTP access from anywhere
   ingress {
@@ -107,28 +144,27 @@ resource "aws_security_group" "web-sg-auto" {
 # INSTANCES #
 resource "aws_instance" "jakl-web-auto" {
   ami                    = data.aws_ami.amazonLinux.id
-  instance_type          = "a1.medium"
+  instance_type          = "t2.small"
   subnet_id              = aws_subnet.web-subnt-auto.id
   vpc_security_group_ids = [aws_security_group.web-sg-auto.id]
   monitoring = true
-
   key_name = "jakl-web-deploy"
   tags = {
     Name = "jakl-web-auto"
   }
-  depends_on = [
-    aws_key_pair.keypair
-  ]
+  #  depends_on = [
+  #    aws_key_pair.keypair
+  #  ]
 
   user_data = <<EOF
-#! /bin/bash
-#sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo usermod -a -G docker ec2-user
-sudo service docker start
-docker login --username aklaimd --password Moople123#
-docker pull aklaimd/jakl-ui:latest
-docker run -d -p 80:80 aklaimd/jakl-ui:latest
-EOF
+  #! /bin/bash
+  #sudo yum update -y
+  sudo amazon-linux-extras install docker
+  sudo usermod -a -G docker ec2-user
+  sudo service docker start
+  docker login --username aklaimd --password Moople123#
+  docker pull aklaimd/jakl-ui:latest
+  docker run -d -p 80:80 aklaimd/jakl-ui:latest
+  EOF
 
 }
